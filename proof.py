6ce4aa042dad371aa45fac2beb18268c12774322
Chinmaya Krishnan Mahesh <cmahesh2@illinois.edu>
2019-02-24 07:42:41 -0600

    improve

--- proof.py
+++ proof.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template
 from proof_parser import *
+from test_code import *
+from transpiler import *
 
 app = Flask(__name__)
 
@@ -10,7 +12,14 @@ def proof_form():
 @app.route('/', methods=['POST'])
 def proof_form_post():
     text = request.form['text']
-    return str(parser(text))
+    a,b,c,d,e = parser(text)
+    parsed_code = parse(a,b,c,d,e)
+    code_file = open("/tmp/code_file.lean", "w")
+    for i in parsed_code:
+	    code_file.write(i + "\n")
+    code_file.close()
+
+    return str(getErrors("/tmp/code_file.lean"))
 
 if __name__== "__main__":
     app.run()
--- proof_parser.py
+++ proof_parser.py
@@ -2,47 +2,38 @@ import nltk
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 
-def get_variables(statement):
-    variables = []
-    for word in statement:
-        if word == "be":
-            break
-        elif word != ",":
-            variables.append(word)
-
-    return variables
-
-def get_sets(statement):
-    sets = []
-    for word in statement:
-        for l in word:
-            if l.isupper():
-                sets.append(l)
-    sets = list(set(sets))
-
-    return sets
 
 def get_assumptions(statement):
-    assumptions = [t.strip() for t in statement.split("such that")[1].split(',')]
-    return assumptions
+	return " ".join(statement.split(" ")[1:])
 
 def parser(proof):
-    sent_token = proof.split('\n', proof.count('\n'))
-    variables = []; sets = []; assumptions = []; statements = []; question=""
+	sent_token = proof.split('\n', proof.count('\n'))
+	variables = []; sets = []; assumptions = []; statements = []; question=""
 
-    for statement in sent_token:
-        if len(statement) < 2:
-            continue
+	for statement in sent_token:
+		words = statement.split(" ")
+		for i in words:
+			if len(i) == 1:
+				if i.isupper():
+					sets.append(i)
+				elif i.islower():
+					variables.append(i)
 
-        if "Question:" in statement:
-            question = statement.split("Question:")[1].strip()
 
-        word_token = word_tokenize(statement)
-        if word_token[0] == "Let":
-            variables = get_variables(word_token[1:])
-            sets = get_sets(word_token[1:])
-            assumptions = get_assumptions(statement)
-        elif statement[0] == "⇒" or statement[0] == "∴":
-            statements.append(statement[1:].rstrip())
+		if len(statement) < 2:
+			continue
 
-    return question, variables, sets, assumptions, statements
+		if "Question:" in statement:
+			question = statement.split("Question:")[1].strip()
+
+		word_token = word_tokenize(statement)
+		if word_token[0] == "Let":
+			assumptions.append(get_assumptions(statement).rstrip())
+		elif statement[0] == "⇒" or statement[0] == "∴":
+			statements.append(statement[1:].rstrip())
+
+	variables = list(set(variables))
+	sets = list(set(sets))
+
+
+	return question, sets, variables, assumptions, statements
