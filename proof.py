f3db6edbcbae2771e0d84b73dc5162ac541a0479
Chinmaya Krishnan Mahesh <cmahesh2@illinois.edu>
2019-02-24 09:21:41 -0600

    Fix indentation

--- proof.py
+++ proof.py
@@ -7,21 +7,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def proof_form():
-    return render_template('proof-form.html')
+	return render_template('proof-form.html')
 
 @app.route('/', methods=['POST'])
 def proof_form_post():
-    text = request.form['text']
-    a,b,c,d,e = parser(text)
-    parsed_code = parse(a,b,c,d,e)
-    code_file = open("/tmp/code_file.lean", "w")
-    for i in parsed_code:
-	    code_file.write(i + "\n")
-    code_file.close()
-    messages = getErrors("/tmp/code_file.len")
+	text = request.form['text']
+	a,b,c,d,e = parser(text)
+	parsed_code = parse(a,b,c,d,e)
+	code_file = open("/tmp/code_file.lean", "w")
+	for i in parsed_code:
+		code_file.write(i + "\n")
+	code_file.close()
+	messages = getErrors("/tmp/code_file.len")
 
-    return render_template('output.html', messages=messages)
-    #return str(getErrors("/tmp/code_file.lean"))
+	return render_template('output.html', messages=messages)
 
 if __name__== "__main__":
-    app.run()
+	app.run()
