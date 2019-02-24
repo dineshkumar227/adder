7fd8a81dcadb93a04641365211d5d03f7c8e8a37
Chinmaya Krishnan Mahesh <cmahesh2@illinois.edu>
2019-02-24 08:40:22 -0600

    Allow lean path to be specified by an environment variable

--- test_code.py
+++ test_code.py
@@ -3,7 +3,8 @@ import sys, os, subprocess
 def getErrors(sourcefile):
 # ~/code/bin/lean-3.4.2-linux/bin/lean --run
 	ret = []
-	pr = subprocess.Popen(['/home/chinmaya/code/bin/lean-3.4.2-linux/bin/lean', '--run', sourcefile],  stdout=subprocess.PIPE, stdin=subprocess.PIPE,stderr=subprocess.PIPE)
+	lean_path = os.environ['LEAN_PATH_PRAIRIE']
+	pr = subprocess.Popen([lean_path, '--run', sourcefile],  stdout=subprocess.PIPE, stdin=subprocess.PIPE,stderr=subprocess.PIPE)
 
 	output, err = pr.communicate()
 
